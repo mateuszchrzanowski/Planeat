331989bb44aeb73b3b143940ba0db1714092d9fb
Mateusz Chrzanowski <matechrzanowski@neuca.pl>
2022-11-01 05:34:24 +0100

    Update UserService.cs and create UserDto.cs (#4)
    - Add Get() method in IUserService.cs
    - Add username parameter in Register() method in UserService.cs
    - Implement Get() method in UserService.cs
    - Implement UserDto.cs in Get() method in UserService.cs

--- Planeat.Infrastructure/Services/IUserService.cs
+++ Planeat.Infrastructure/Services/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Planeat.Infrastructure.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace Planeat.Infrastructure.Services
 {
     public interface IUserService
     {
-        void Register(string email, string password);
+        UserDto Get(string email);
+        void Register(string email, string username, string password);
     }
 }
