3f4dc8a75ef26f1340f06df5909b2f93f94530ff
Mateusz Chrzanowski <matechrzanowski@neuca.pl>
2022-11-11 06:30:23 +0100

    Tests and Autofac (#6, #17).
    - Add packages references for testing in Planeat.Tests and Planeat.Tests.EndToEnd,
    - Create first tests fo UserService and UserController,
    - Add Autofac package reference.

---
+++ Planeat.Tests/Services/UserServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Moq;
+using Planeat.Core.Domain;
+using Planeat.Core.Repositories;
+using Planeat.Infrastructure.Repositories;
+using Planeat.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Planeat.Tests.Services
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public async Task RegisterAsync_AddValidUser_ShouldAddAsyncOnUserRepository()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var mapperMock = new Mock<IMapper>();
+
+            var userService = new UserService(userRepositoryMock.Object, mapperMock.Object);
+            await userService.RegisterAsync("userTest@user.com", "userTest", "secretTest");
+
+            userRepositoryMock.Verify(x => x.AddAsync(It.IsAny<User>()), Times.Once);
+        }
+
+        //[Fact]
+        //public async Task RegisterAsync_AddUserWithExistingEmail_ShouldThrowsException()
+        //{
+        //}
+
+        //[Fact]
+        //public async Task GetAsync_WithValidEmail_ShouldInvokeGetAsyncOnUserRepositoryAndReturnsUser()
+        //{
+        //}
+
+        //[Fact]
+        //public async Task GetAsync_WithInvalidEmail_ShouldInvokeGetAsyncOnUserRepositoryAndReturns404()
+        //{
+        //}
+    }
+}
