40dd8da1ff3cacd559232602a410b01ec815a82e
Mateusz Chrzanowski <matechrzanowski@neuca.pl>
2022-11-15 05:40:51 +0100

    Implement CommandHandler pattern.
    - Add ICommand, ICommandDispatcher, CommandDispatcher, ICommandHandler,
    - Implement Commands: ChangeUserPassword and CreateUser,
    - Implement Handlers: ChangeUserPasswordHandler and CreateUserHandler.

---
+++ Planeat.Infrastructure/Handlers/User/CreateUserHandler.cs
@@ -0,0 +1,26 @@
+﻿using Planeat.Infrastructure.Commands;
+using Planeat.Infrastructure.Commands.User;
+using Planeat.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planeat.Infrastructure.Handlers.User
+{
+    public class CreateUserHandler : ICommandHandler<CreateUser>
+    {
+        private readonly IUserService _userService;
+
+        public CreateUserHandler(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task HandleAsync(CreateUser command)
+        {
+            await _userService.RegisterAsync(command.Email, command.Username, command.Password);
+        }
+    }
+}
