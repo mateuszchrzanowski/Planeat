ecd7863c5dae758ddd465001111f0b92e4b2b0d7
Mateusz Chrzanowski <matechrzanowski@neuca.pl>
2022-11-15 05:37:03 +0100

    Congigure Autofac.
    - Update Program.cs,
    - Update Startup.cs,
    - Add CommandModule and register it in Startup.cs.

---
+++ Planeat.Infrastructure/IcC/Modules/CommandModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Planeat.Infrastructure.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planeat.Infrastructure.IcC.Modules
+{
+    public class CommandModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var assembly = typeof(CommandModule).GetTypeInfo().Assembly;
+
+            builder.RegisterAssemblyTypes(assembly).AsClosedTypesOf(typeof(ICommandHandler<>)).InstancePerLifetimeScope();
+            
+            builder.RegisterType<CommandDispatcher>().As<ICommandDispatcher>().InstancePerLifetimeScope();
+        }
+    }
+}
